4134e893795041ceae158c296f53cb3023d46a38
falkom <falkom@rpi.edu>
2018-03-27 17:39:11 -0400

    Working on AbstractManager and GLFWManager
    Committing my progress so far.
    There is a distinct possibility that I might have to undo all work done for the GLFWManager and AbstractManager so far, but I am hoping this is not the case.
    
    As to what I have done, I am trying to maintain as much of the code that is already in place, just moving it around and tweaking tiny things.

--- Source/Display/Window/AbstractManager.cpp
+++ Source/Display/Window/AbstractManager.cpp
@@ -1,11 +1,15 @@
 #include "AbstractManager.h"
 
-void AbstractManager::GetWindowSize(Window * window, uint & height, uint & width) const {}
-
-void AbstractManager::GetWindowPos(Window * window, uint & x, uint & y) const {
-
+void AbstractManager::GetWindowSize(Window* window, uint& height, uint& width) const {
+	height = window->height;
+	width = window->width;
 }
 
-std::string AbstractManager::GetWindowTitle(Window * window) const {
+void AbstractManager::GetWindowPos(Window* window, uint& x, uint& y) const {
+	x = window->xPos;
+	y = window->yPos;
+}
+
+std::string AbstractManager::GetWindowTitle(Window* window) const {
 	return window->title;
 }
